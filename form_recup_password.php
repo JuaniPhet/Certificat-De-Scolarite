d447efe6bfb682744745a9b70597e135bbc9bbf1
Japhet SEUMO <seumojaphet555@gmail.com>
2023-02-28 19:26:59 +0100

    updates

--- form_recup_password.php
+++ form_recup_password.php
@@ -1,20 +1,20 @@
 <?php 
-session_start();
-require_once('dbconnexion.php');
-@$matricule=isset($_POST['matricule-recup'])?$_POST['matricule-recup']:"";
-@$nom=isset($_POST['nom-recup'])?$_POST['nom-recup']:"";
-@$datenaiss=isset($_POST['datenaissance-recup'])?$_POST['datenaissance-recup']:"";
-$requete="select * from users where matricule='$matricule'and datedenaissance='$datenaiss'" ;
-$resultat=$pdo->query($requete);
-if ($user=$resultat->fetch()){
-    if($user['etat']==1 && $user['nom'].' '.$user['prenom']==$nom){
-        $passRecup = $user['motdepasse'];
+    session_start();
+    require_once('dbconnexion.php');
+    @$matriculeRecup=isset($_POST['matricule-recup'])?$_POST['matricule-recup']:"";
+    @$nom=isset($_POST['nom-recup'])?$_POST['nom-recup']:"";
+    @$datenaiss=isset($_POST['datenaissance-recup'])?$_POST['datenaissance-recup']:"";
+    $requete="select * from users where matricule='$matriculeRecup'and datedenaissance='$datenaiss'";
+    $resultat=$pdo->query($requete);
+    if($user=$resultat->fetch()){
+        if($user['etat']==1 && $user['nom'].' '.$user['prenom']==$nom){
+            $passRecup=$user['motdepasse'];
+        }
+        else{
+            $_SESSION['errlogin']="<strong>Erreur</strong>Votre Compte est desactivé";
+            header('location:index.php');
+        }
     }
-    else{
-        $_SESSION['errlogin']="<strong>Erreur</strong>Votre Compte est desactivé";
-        header('location:index.php');
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -30,8 +30,8 @@ if ($user=$resultat->fetch()){
 
 <body>
     <h3>Vous souhaitez récuperer votre mot de passe ?</h3>
-    <div id="container-index">
-        <form action="form_recup_password.php" method="post" id="form-recup">
+    <div id="container-recup">
+        <form action="" method="post" id="form-recup">
             <label for="matricule-recup">Matricule</label><sup>*</sup><br>
             <input type="text" name="matricule-recup" id="matricule-recup" class="inputs"
                 placeholder="Entrez votre matricule"><br>
@@ -47,7 +47,7 @@ if ($user=$resultat->fetch()){
             <div id="btn">
                 <input type="submit" value="Envoyer" class="btns" id="connect-btn">
             </div>
-            <div id="fgt-pwd">"Votre mot de passe est <b><?php print_r($passRecup)?></b>. Copiez le et <a href="index.php">quittez ici</a></div>
+            <div id="fgt-pwd">Votre mot de passe est <b><?php print_r($passRecup)?></b>. Copiez le et <a href="index.php">quittez ici</a></div>
             </form>
         </div>
         <script src="js\script_recup.js"></script>
